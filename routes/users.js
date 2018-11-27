5ca3a4ca9a53754da6fac4c329a2f7ed64d54a35
SoyaManabe <soyamanabe22@gmail.com>
2018-11-27 10:25:29 +0000

    プロフィール機能追加

--- routes/users.js
+++ routes/users.js
@@ -35,4 +35,74 @@ router.get('/:userId', authenticationEnsurer, (req, res, next) => {
     });
 });
 
+//今回やったところ！！！！
+router.get('/:userId/edit', authenticationEnsurer, (req, res, next) => {
+    User.findOne({
+        where: {
+            userId: req.params.userId
+        }
+    }).then((user) => {
+        if (isMine(req, user)) {
+            res.render('profile', {
+                user: user
+            });
+        } else {
+            const err = new Error('You are not authorized to edit this profile');
+            err.status = 404;
+            next(err);
+        }
+    });
+});
+
+
+
+router.post('/:userId', authenticationEnsurer, (req, res, next) => {
+    console.log(req.body.profile);
+    Book.findAll({
+        where: {
+            createdBy: req.params.userId
+        },
+        order: [['"updatedAt"', 'DESC']]
+    }).then((books) => {
+        if (books) {
+            User.findOne({
+                where: {
+                    userId: req.params.userId
+                }
+            }).then((user) => {
+                if (user && isMine(req, user)) {
+                    if (parseInt(req.query.edit) === 1) {
+                        user.update({
+                            profile: req.body.profile
+                        });
+                        res.render('user', {
+                            books: books,
+                            //user: req.user,
+                            user: user,
+                            geekid: req.params.userId,
+                            geek: user
+                        });
+                    } else {
+                        const err = new Error('Bad request');
+                        err.status = 400;
+                        next(err);
+                    }
+                } else {
+                    const err = new Error('You are not authorized');
+                    err.status = 404;
+                    next(err);
+                }
+            });
+        } else {
+            const err = new Error('まだほんの投稿がされていないユーザーです');
+            err.status = 404;
+            next(err);
+        }
+    });
+});
+
+function isMine(req, user) {
+    return user && parseInt(user.userId) === parseInt(req.user.id);
+}
+
 module.exports = router;
